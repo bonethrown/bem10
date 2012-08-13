e6f1b2f09de281d5f84ff02d1f725294db6d1300
ben <opinion.blogger@gmail.com>
2012-08-13 09:48:32 +0300

    test commit
    rest for ricardo commit

---
+++ bem10/mainItem.h
@@ -0,0 +1,20 @@
+//
+//  mainItem.h
+//  bem10
+//
+//  Created by kaya tilev on 8/10/12.
+//  Copyright (c) 2012 kaya tilev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mainItem : NSObject
+
+@property (nonatomic, copy) NSString *userName;
+
+@property (nonatomic, copy) NSString *itemKey;
+
+
+
+
+@end
---
+++ bem10/mainItem.m
@@ -0,0 +1,15 @@
+//
+//  mainItem.m
+//  bem10
+//
+//  Created by kaya tilev on 8/10/12.
+//  Copyright (c) 2012 kaya tilev. All rights reserved.
+//
+
+#import "mainItem.h"
+
+@implementation mainItem
+
+
+
+@end
